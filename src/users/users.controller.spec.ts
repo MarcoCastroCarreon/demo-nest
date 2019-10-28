2ac90f59015d0df12b491bc9cab1ba313332be4b
Marco Castro <mcastro@mediomelon.mx>
2019-10-27 20:08:51 -0600

    trying to test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserStatus } from 'src/common/enums/user-status.enum';
 
 describe('Users Controller', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
+  
+  describe('getAll', () => {
+    it('it should return an array of users', async () => {
+      const user = { id: 1, name: 'Pedro El Guapo', email: 'penriquez@mediomelon.mx', status: UserStatus.ENABLED };
+      const result = [user];
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+      jest.spyOn(service, 'findAll');
+
+      expect(await controller.getAll()).toBe(result);
+    })
+  })
 });
